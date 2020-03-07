c872abfac1d5202bb673993d0a9dfdb8da4cfa80
pacokwon <haechank@gmail.com>
2020-03-07 12:40:33 +0900

    add - boilerplate code for react, redux

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard.js'
+
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Dashboard}/>
+        </Switch>
+    </BrowserRouter>
+)
+
+export default App
