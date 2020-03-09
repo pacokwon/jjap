bc3af5255294f44eeb2db37b8d78ef0f62d41782
pacokwon <haechank@gmail.com>
2020-03-09 11:32:49 +0900

    add - boilerplate code for bodyparser and mongoose

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
 import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
 
+const app = express()
+const db = mongoose.connection
+db.once('open', () => console.log('connected to mongodb'))
+mongoose.connect('mongodb://localhost/JLPT', { useNewUrlParser: true, useFindAndModify: false });
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => {
+    console.log('Listening on localhost:5000')
+})
