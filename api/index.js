b346a053758272cbed292569b85cf7d5217a5dab
pacokwon <haechank@gmail.com>
2020-03-11 00:17:35 +0900

    update - remove mongoose imports

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import api from './src/routes/api.js'
 
 const app = express()
-const db = mongoose.connection
-db.once('open', () => console.log('connected to mongodb'))
-mongoose.connect('mongodb://localhost/JLPT', { useNewUrlParser: true, useFindAndModify: false });
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,5 +10,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', api)
 
 app.listen(8080, () => {
-    console.log('Listening on localhost:5000')
+    console.log('Listening on localhost:8080')
 })
