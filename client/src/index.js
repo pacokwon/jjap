3527c494741ba7881e77c39eb134b86f5366b8c1
pacokwon <haechank@gmail.com>
2020-03-11 01:46:54 +0900

    bugfix - change import directory

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App.js'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import rootReducer from '../reducers'
+import rootReducer from './reducers'
 
 const store = createStore(rootReducer)
 ReactDOM.render(
