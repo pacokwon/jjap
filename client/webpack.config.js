7114a53837625684a43f8fd3cfb808c9647b9a1b
pacokwon <haechank@gmail.com>
2020-03-16 01:37:31 +0900

    add - webpack apihistoryfallback config for routing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,31 +2,33 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "bundle.js"
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    port: 8081,
+    host: '0.0.0.0',
+    hot: true,
+    proxy: {
+      '/api': 'http://server:8000/'
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-    devServer: {
-        port: 8081,
-        host: '0.0.0.0',
-        hot: true,
-        proxy: {
-            '/api': 'http://server:8000/'
-        }
-    }
+    historyApiFallback: true
+  }
 }
